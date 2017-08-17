111c9963d71452c8c430b0b6998fa5a1e6205419
hellanurag <30447996+hellanurag@users.noreply.github.com>
2017-08-17 14:28:08 -0700

    First SimpleNLG test file

---
+++ src/TestMain.java
@@ -0,0 +1,17 @@
+import simplenlg.framework.*;
+import simplenlg.lexicon.*;
+import simplenlg.realiser.english.*;
+import simplenlg.phrasespec.*;
+import simplenlg.features.*;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Lexicon lexicon = Lexicon.getDefaultLexicon();
+        NLGFactory nlgFactory = new NLGFactory(lexicon);
+        Realiser realiser = new Realiser(lexicon);
+
+        NLGElement s1 = nlgFactory.createSentence("my dog is happy");
+        String output = realiser.realiseSentence(s1);
+        System.out.println(output);
+}
+}
