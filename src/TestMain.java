1048d0786efa6f13c893cb0b1261becc33a9710f
hellanurag <30447996+hellanurag@users.noreply.github.com>
2017-08-17 15:59:05 -0700

    Variations of SimpleNLG

--- src/TestMain.java
+++ src/TestMain.java
@@ -13,5 +13,33 @@ public class TestMain {
         NLGElement s1 = nlgFactory.createSentence("my dog is happy");
         String output = realiser.realiseSentence(s1);
         System.out.println(output);
+        
+        SPhraseSpec p = nlgFactory.createClause();
+        p.setSubject("Mary");
+        p.setVerb("chase");
+        p.setObject("the monkey");
+        p.setFeature(Feature.TENSE, Tense.FUTURE);
+        p.setFeature(Feature.NEGATED, true);
+        p.addComplement("very quickly"); // Adverb phrase, passed as a string
+        p.addComplement("because she is exhausted"); // Prepositional phrase, string
+        
+        //Alternative way to do things. Might be better.
+        NPPhraseSpec subject1 = nlgFactory.createNounPhrase("Mary");
+        NPPhraseSpec subject2 = nlgFactory.createNounPhrase("her", "dog");
+
+        CoordinatedPhraseElement subj = nlgFactory.createCoordinatedPhrase(subject1, subject2); 
+        // may revert to nlgFactory.createCoordinatedPhrase( subject1, subject2 ) ;
+        p.setSubject(subj);
+        
+        NPPhraseSpec object1 = nlgFactory.createNounPhrase("the monkey");
+        NPPhraseSpec object2 = nlgFactory.createNounPhrase("George");
+
+        CoordinatedPhraseElement obj = nlgFactory.createCoordinatedPhrase(object1, object2); 
+        obj.addCoordinate("Martha");
+        p.setObject(obj);
+        obj.setFeature(Feature.CONJUNCTION, "or");
+        
+        String output2 = realiser.realiseSentence(p); // Realiser created earlier.
+        System.out.println(output2);
 }
 }
