93a32992e42a7bfb73d4ed0f4bfb637a88fe88e4
hellanurag <30447996+hellanurag@users.noreply.github.com>
2017-08-17 16:16:43 -0700

    multiple clauses and paragraphs

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import simplenlg.framework.*;
 import simplenlg.lexicon.*;
 import simplenlg.realiser.english.*;
@@ -10,8 +11,8 @@ public class TestMain {
         NLGFactory nlgFactory = new NLGFactory(lexicon);
         Realiser realiser = new Realiser(lexicon);
 
-        NLGElement s1 = nlgFactory.createSentence("my dog is happy");
-        String output = realiser.realiseSentence(s1);
+        NLGElement s = nlgFactory.createSentence("my dog is happy");
+        String output = realiser.realiseSentence(s);
         System.out.println(output);
         
         SPhraseSpec p = nlgFactory.createClause();
@@ -39,7 +40,43 @@ public class TestMain {
         p.setObject(obj);
         obj.setFeature(Feature.CONJUNCTION, "or");
         
+        NPPhraseSpec place = nlgFactory.createNounPhrase("park");
+        place.setDeterminer("the");
+        PPPhraseSpec pp = nlgFactory.createPrepositionPhrase();
+        pp.addComplement(place);
+        pp.setPreposition("in");
+        place.addPreModifier("leafy");
+        p.addComplement(pp);
+        
         String output2 = realiser.realiseSentence(p); // Realiser created earlier.
         System.out.println(output2);
+        
+        SPhraseSpec q = nlgFactory.createClause("I", "be", "happy");
+        SPhraseSpec r = nlgFactory.createClause("I", "eat", "fish");
+        
+        r.setFeature(Feature.COMPLEMENTISER, "because");
+        r.setFeature(Feature.TENSE, Tense.PAST);
+        q.addComplement(r);
+                
+        String output3 = realiser.realiseSentence(q);  //Realiser created earlier
+        System.out.println(output3);
+        
+        SPhraseSpec p1 = nlgFactory.createClause("Mary", "chase", "the monkey");
+        SPhraseSpec p2 = nlgFactory.createClause("The monkey", "fight back"); 
+        SPhraseSpec p3 = nlgFactory.createClause("Mary", "be", "nervous");
+        
+        DocumentElement s1 = nlgFactory.createSentence(p1);
+        DocumentElement s2 = nlgFactory.createSentence(p2);
+        DocumentElement s3 = nlgFactory.createSentence(p3);
+        
+        DocumentElement par1 = nlgFactory.createParagraph(Arrays.asList(s1, s2, s3));
+        String output4 = realiser.realise(par1).getRealisation();
+        System.out.println(output4);
+        
+        DocumentElement section = nlgFactory.createSection("The Trials and Tribulations of Mary and the Monkey");
+        section.addComponent(par1);
+        String output5 = realiser.realise(section).getRealisation();
+        System.out.println(output5);
+        
 }
 }
