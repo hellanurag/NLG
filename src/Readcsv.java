af7d101609b1647d44b919011bd3b387f0100686
hellanurag <30447996+hellanurag@users.noreply.github.com>
2017-08-17 20:57:08 -0700

    customer and Readcsv classes added

---
+++ src/Readcsv.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Readcsv {
+
+
+	public static void main(String[] args) throws IOException {
+		// open file input stream
+		BufferedReader reader = new BufferedReader(new FileReader(
+				"licensedata.csv"));
+				//"C:\\Users\\anurag.reddy\\Documents\\GitHub\\NLG\\licensedata.csv"));
+
+		// read file line by line
+		String line = null;
+		Scanner scanner = null;
+		int index = 0;
+		int iter = 0;
+		List<customer> custList = new ArrayList<>();
+		
+		while ((line = reader.readLine()) != null) {
+			//Skipping the header.
+			if (iter == 0)
+			{
+				iter++;
+				continue;
+			}
+			customer cust = new customer();
+			scanner = new Scanner(line);
+			scanner.useDelimiter(",");
+			while (scanner.hasNext()) {
+				String data = scanner.next();
+				if (index == 0)
+					cust.setUniqueid(Integer.parseInt(data));
+				else if (index == 1)
+					cust.setCutomerid(Integer.parseInt(data));
+				else if (index == 2)
+					cust.setCustomername(data);
+				else if (index == 3)
+					cust.setCustomerrep(data);
+				else if (index == 4)
+					cust.setProductid(Integer.parseInt(data));
+				else if (index == 5)
+					cust.setProductname(data);
+				else if (index == 6)
+					cust.setTotallicenses(Integer.parseInt(data));
+				else if (index == 7)
+					cust.setActiveusers(Integer.parseInt(data));
+				else if (index == 8)
+					cust.setWeeksfrompurchase(Integer.parseInt(data));
+				else
+					System.out.println("invalid data::" + data);
+				index++;
+			}
+			index = 0;
+			custList.add(cust);
+			iter++;
+		}
+		
+		//close reader
+		reader.close();
+		
+		//System.out.println(custList.get(0).getUniqueid());
+		
+	}
+
+}
---
+++ src/customer.java
@@ -0,0 +1,73 @@
+
+public class customer {
+
+	//customer fields
+	private int 	uniqueid;
+	private int		cutomerid;
+	private String	customername;
+	private String	customerrep;
+	private int		productid;
+	private String	productname;
+	private int		totallicenses;
+	private int		activeusers;
+	private int		weeksfrompurchase;
+	
+	//Setters and Getters
+	public int getUniqueid() {
+		return uniqueid;
+	}
+	public void setUniqueid(int uniqueid) {
+		this.uniqueid = uniqueid;
+	}
+	public int getCutomerid() {
+		return cutomerid;
+	}
+	public void setCutomerid(int cutomerid) {
+		this.cutomerid = cutomerid;
+	}
+	public String getCustomername() {
+		return customername;
+	}
+	public void setCustomername(String customername) {
+		this.customername = customername;
+	}
+	public String getCustomerrep() {
+		return customerrep;
+	}
+	public void setCustomerrep(String customerrep) {
+		this.customerrep = customerrep;
+	}
+	public int getProductid() {
+		return productid;
+	}
+	public void setProductid(int productid) {
+		this.productid = productid;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public int getTotallicenses() {
+		return totallicenses;
+	}
+	public void setTotallicenses(int totallicenses) {
+		this.totallicenses = totallicenses;
+	}
+	public int getActiveusers() {
+		return activeusers;
+	}
+	public void setActiveusers(int activeusers) {
+		this.activeusers = activeusers;
+	}
+	public int getWeeksfrompurchase() {
+		return weeksfrompurchase;
+	}
+	public void setWeeksfrompurchase(int weeksfrompurchase) {
+		this.weeksfrompurchase = weeksfrompurchase;
+	}
+	
+	
+	
+}
